1939cd7175e0bb79d6902bd69f8040a0eb43936e
eamonpenland <eamon.penland@gmail.com>
2017-09-19 00:04:26 -0400

    updated goth

--- lib/cloud_vision.ex
+++ lib/cloud_vision.ex
@@ -1,5 +1,4 @@
 defmodule CloudVision do
-  alias Goth.Token
   @features [label: "LABEL_DETECTION", logo: "LOGO_DETECTION", text: "TEXT_DETECTION",
    face: "FACE_DETECTION", landmark: "LANDMARK_DETECTION", safe_search: "SAFE_SEARCH_DETECTION",
    image_properties: "IMAGE_PROPERTIES", unspecified: "TYPE_UNSPECIFIED"
